968e4264adb0a6a3b000322341b819f00a736a75
dennyzain <abbasdenny24@gmail.com>
2022-06-22 21:16:07 +0700

    fixing the wrong tuple types

--- tuple.py
+++ tuple.py
@@ -1,7 +1,7 @@
 # define a tuple
-coordinateY=20.0
-coordinateX=10.0
+# coordinateY=20.0
+# coordinateX=10.0
 
-coordinate= {20.0,10.0}
+coordinate= (20.0,10.0)
 
-print(coordinate)
+print(coordinate[0]+coordinate[1])
